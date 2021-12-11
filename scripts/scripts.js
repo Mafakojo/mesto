0516ea924e6563cfd66757511d078315f7af0f5d
Mafakoja <358mnogo@gmail.com>
2021-12-11 16:25:41 +0600

    feat: добавил редактирование профиля

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,16 +1,30 @@
 const editButton = document.querySelector(".profile__edit-button");
-
 const popup = document.querySelector(".popup");
-
 const closeButton = document.querySelector(".popup__close-button");
+const formElement = document.querySelector("#popup__form");
+let nameInput = document.querySelector(".popup__profile_name");
+let jobInput = document.querySelector(".popup__profile_profession");
+let nameProfile = document.querySelector(".profile__info-name");
+let jobProfile = document.querySelector(".profile__info-profession");
 
 function openPopup () {
-  popup.classList.add("popup__active");
+  popup.classList.add("popup_opened");
+  nameInput.value = nameProfile.textContent;
+  jobInput.value = jobProfile.textContent;
 }
-
-editButton.addEventListener("click", openPopup);
 
 function closePopup () {
-  popup.classList.remove ("popup__active");
+  popup.classList.remove ("popup_opened");
 }
+
+function formSubmitHandler (evt) {
+  evt.preventDefault();
+nameProfile.textContent = nameInput.value;
+jobProfile.textContent = jobInput.value;
+popup.classList.remove ("popup_opened");
+}
+
+
+editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
