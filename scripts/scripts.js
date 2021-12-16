a28bcee6a81a4f65b03959e848694e1ea24f6e66
Mafakoja <358mnogo@gmail.com>
2021-12-16 22:40:55 +0600

    fix: поправил попа ап

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -7,23 +7,22 @@ let jobInput = document.querySelector(".popup__profile_profession");
 let nameProfile = document.querySelector(".profile__info-name");
 let jobProfile = document.querySelector(".profile__info-profession");
 
-function openPopup () {
+function openPopup() {
   popup.classList.add("popup_opened");
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
 }
 
-function closePopup () {
-  popup.classList.remove ("popup_opened");
+function closePopup() {
+  popup.classList.remove("popup_opened");
 }
 
-function formSubmitHandler (evt) {
+function formSubmitHandler(evt) {
   evt.preventDefault();
-nameProfile.textContent = nameInput.value;
-jobProfile.textContent = jobInput.value;
-popup.classList.remove ("popup_opened");
+  nameProfile.textContent = nameInput.value;
+  jobProfile.textContent = jobInput.value;
+  popup.classList.remove("popup_opened");
 }
-
 
 editButton.addEventListener("click", openPopup);
 closeButton.addEventListener("click", closePopup);
