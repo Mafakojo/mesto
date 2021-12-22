fd1235eb3228e5f86bb73d1242829c0bfb70cf54
Mafakoja <358mnogo@gmail.com>
2021-12-22 19:24:23 +0600

    fix: поправил переполнение блока job, дублирование функции в JS

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,39 +1,30 @@
-let editButton = document.querySelector('.profile__edit-button');
-let closeButton = document.querySelector('.popup__close-button');
-let popup = document.querySelector('.popup');
-let formElement = document.querySelector('#popup__form');
-let nameInput = document.querySelector('.popup__input_data_name');
-let jobInput = document.querySelector('.popup__input_data_job');
-let nameProfile = document.querySelector('.profile__info-name');
-let jobProfile = document.querySelector('.profile__info-profession');
-let cardsButton = document.querySelector('.cards__button');
+let editButton = document.querySelector(".profile__edit-button");
+let closeButton = document.querySelector(".popup__close-button");
+let popup = document.querySelector(".popup");
+let formElement = document.querySelector("#popup__form");
+let nameInput = document.querySelector(".popup__input_data_name");
+let jobInput = document.querySelector(".popup__input_data_job");
+let nameProfile = document.querySelector(".profile__info-name");
+let jobProfile = document.querySelector(".profile__info-profession");
+let cardsButton = document.querySelector(".cards__button");
 
 function openPopup() {
-  popup.classList.add('popup_opened');
   nameInput.value = nameProfile.textContent;
   jobInput.value = jobProfile.textContent;
+  popup.classList.add("popup_opened");
 }
 
 function closePopup() {
-  popup.classList.remove('popup_opened');
-}
-
-function likeButton() {
-  cardsButton.classList.add('cards__button_active');
-}
-
-function dislikeButton() {
-  cardsButton.classList.remove('cards__button_active');
+  popup.classList.remove("popup_opened");
 }
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   nameProfile.textContent = nameInput.value;
   jobProfile.textContent = jobInput.value;
-  popup.classList.remove('popup_opened');
+  closePopup();
 }
 
-editButton.addEventListener('click', openPopup);
-closeButton.addEventListener('click', closePopup);
-formElement.addEventListener('submit', formSubmitHandler);
-cardsButton.addEventListener('click', likeButton);
+editButton.addEventListener("click", openPopup);
+closeButton.addEventListener("click", closePopup);
+formElement.addEventListener("submit", formSubmitHandler);
